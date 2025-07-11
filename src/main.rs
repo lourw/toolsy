18859ec3f8d94a2b27f1dec3aaa0f7de73cec56c
lourw <56288712+lourw@users.noreply.github.com>
2025-07-10 22:55:57 -0700

    feat(json): add basic commands for json feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use clap::{ Parser, Subcommand };
+
+#[derive(Parser)]
+#[command(version, about, long_about=None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+     
+    #[arg(short, long)]
+    debug: bool
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Json {
+        #[command(subcommand)]
+        command: Option<JsonCommand>
+    }
+}
+
+#[derive(Subcommand)]
+enum JsonCommand {
+    Format {
+
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+     
+    if cli.debug {
+        println!("Debug mode is ON");
+    }
+
+    match &cli.command {
+        Some(Command::Json {command}) => {
+            match *command {
+                Some(JsonCommand::Format {}) => {
+                    println!("Running some format command")
+                }
+                None => {}
+            }
+        }
+        None => {}
+    }
 }
